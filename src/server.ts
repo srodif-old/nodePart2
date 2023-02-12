42c6c0918a8c21de5c8f6a5a65b2c8cd8120bc8d
srodiftsis <srodiftsis@gmail.com>
2023-02-12 15:54:54 +0200

    express01

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,51 @@
-const TODO: string = "start writing your Express API server here :)";
+import express from 'express';
+import 'express-async-errors';
+import morgan from 'morgan';
 
-console.log(TODO);
+const app = express()
+const port = 3000
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+type Planet = {
+    id: number,
+    name: string,
+  };
+
+type Planets = Planet[];
+
+let planets: Planets = [
+    {
+      id: 1,
+      name: "Earth",
+    },
+    {
+      id: 2,
+      name: "Mars",
+    },
+];
+
+
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!')
+})
+
+app.get('/api/planets', (req, res) => {
+    res.status(200).json(planets);
+})
+
+app.get('/api/planets/:id', (req, res) => {
+    const { id } = req.params;
+    const planet = planets.find( p => p.id == Number(id));
+    res.status(200).json(planet);
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
